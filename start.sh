770c85a4f5c30b6a43661f5adfffd1ce07f58cf1
singyun4970 <singyun4970@gmail.com>
2026-05-15 21:10:59 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-wget https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar -O server.jar
+curl -L https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar -o server.jar
 
 java -Xmx1G -jar server.jar nogui
